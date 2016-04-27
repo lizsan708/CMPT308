3570713caf63279bd7a2047c8d71baf40fab7a49
lizsan708 <lizsan708@yahoo.com>
2016-04-27 17:27:21 -0400

    Lizeth Sanchez DBMan Lab 10

---
+++ Lizeth Sanchez DBMan Lab 10.sql
@@ -0,0 +1,45 @@
+﻿--Lizeth Sanchez
+--Database Management
+--Professor Rivas
+--27 April 2016
+
+--PreReqsFor--
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   numInput   int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select p.courseNum, p.preReqNum
+      from   Prerequisites p, Courses c
+      where  c.num = p.PreReqNum
+      and    numInput = p.courseNum;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select PreReqsFor(499, 'results');
+Fetch all from results;
+
+
+--IsPreReqFor--
+create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   numInput   int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select p.preReqNum, p.courseNum
+      from   Prerequisites p, Courses c
+      where  p.courseNum = c.num
+      and    numInput = p.PreReqNum;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select IsPreReqFor(120, 'results');
+Fetch all from results;
